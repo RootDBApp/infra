34a5c2d1af1a4be89ebbbaf7a6be03f901025e76
spo <sebastien.porquet@atomicweb.fr>
2024-03-29 17:29:58 +0100

    #2 - Update templates configs, like nginx & app for Reverb.

--- bash/app-config.js
+++ bash/app-config.js
@@ -1,13 +1,7 @@
 window.appConfig = {
-    'REACT_APP_API_URL': 'http://api.hostname.tld',
-    'REACT_APP_ECHO_CLIENT_KEY': 'jetuePoh5uyeeth8oom1Aeg8Lae7guchie2ieba3ahyai',
-    'REACT_APP_ECHO_CLIENT_CLUSTER': 'rootdb-api-localhost-cluster',
-    'REACT_APP_ECHO_CLIENT_AUTHENDPOINT': 'http://api.hostname.tld/broadcasting/auth',
-    'REACT_APP_ECHO_CLIENT_BROADCASTE': 'pusher',
-    'REACT_APP_ECHO_CLIENT_WS_HOST': 'ws_api.hostname.tld',
-    'REACT_APP_ECHO_CLIENT_WS_PORT': '80',
-    'REACT_APP_ECHO_CLIENT_WSS_HOST': 'ws_api.hostname.tld',
-    'REACT_APP_ECHO_CLIENT_WSS_PORT': '80',
-    'WS_SOCKET_PORT': '80',
-    'WDS_SOCKET_PORT': '80'
+    'REACT_APP_API_URL': 'https://api.hostname.tld',
+    'REACT_APP_VITE_REVERB_APP_KEY': 'jetuePoh5uyeeth8oom1Aeg8Lae7guchie2ieba3ahyai',
+    'REACT_APP_VITE_REVERB_HOST': 'ws_api.hostname.tldr',
+    'REACT_APP_VITE_REVERB_PORT': '443',
+    'REACT_APP_VITE_REVERB_SCHEME': 'https',
 };
