ff2e7b76784b7adfac65dc0d030953e3e7436947
spo <sebastien.porquet@atomicweb.fr>
2024-03-29 18:00:53 +0100

    #2 - Update new test docker images.

--- docker-compose/rdb_mariadb_memcached/app-config.js
+++ docker-compose/rdb_mariadb_memcached/app-config.js
@@ -1,13 +1,7 @@
 window.appConfig = {
     'REACT_APP_API_URL': 'http://localhost:8090',
-    'REACT_APP_ECHO_CLIENT_KEY': 'jetuePoh5uyeeth8oom1Aeg8Lae7guchie2ieba3ahyai',
-    'REACT_APP_ECHO_CLIENT_CLUSTER': 'rootdb-api-localhost-cluster',
-    'REACT_APP_ECHO_CLIENT_AUTHENDPOINT': 'http://localhost:8090/broadcasting/auth',
-    'REACT_APP_ECHO_CLIENT_BROADCASTE': 'pusher',
-    'REACT_APP_ECHO_CLIENT_WS_HOST': 'localhost',
-    'REACT_APP_ECHO_CLIENT_WS_PORT': '8081',
-    'REACT_APP_ECHO_CLIENT_WSS_HOST': 'localhost',
-    'REACT_APP_ECHO_CLIENT_WSS_PORT': '8081',
-    'WS_SOCKET_PORT': '8091',
-    'WDS_SOCKET_PORT': '8091'
+    'REACT_APP_VITE_REVERB_APP_KEY': 'jetuePoh5uyeeth8oom1Aeg8Lae7guchie2ieba3ahyai',
+    'REACT_APP_VITE_REVERB_HOST': 'localhost',
+    'REACT_APP_VITE_REVERB_PORT': '8081',
+    'REACT_APP_VITE_REVERB_SCHEME': 'http',
 };
